d00ad25f53ee76488ebd56322e262186190f28eb
Anjali Tiwari <tiwarianjali9000@gmail.com>
2026-02-18 00:16:00 +0530

    deploy steps

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
 import { motion } from "framer-motion";
 
-const API = "http://localhost:5000";
+const API = import.meta.env.VITE_API_URL;
 
 
 export default function App() {
